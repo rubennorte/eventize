f69b351a4b65b976b375f044d8f31b6176117197
Rubén Norte <rubennorte@gmail.com>
2015-03-15 16:47:34 +0100

    Added configuration to launch tests in Karma for browsers

---
+++ tests/unit/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Index file to generate the test bundle for browsers
+ */
+
+require('./eventize-method.spec');
+require('./eventize-methods.spec');
+require('./eventize-object.spec');
+require('./eventize.spec');
+require('./events.spec');

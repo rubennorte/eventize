0bcbd6cec18da0aa80dcdc8c6e627020d2ff0816
Rubén Norte <rubennorte@gmail.com>
2015-03-19 22:40:59 +0100

    New version: 0.7.0

--- dist/eventize.js
+++ dist/eventize.js
@@ -3,6 +3,15 @@
 'use strict';
 
 /**
+ * Converts an array-like object into a real array
+ * @param  {Object} arrayLike
+ * @return {Array}
+ */
+function toArray(arrayLike) {
+  return [].slice.call(arrayLike);
+}
+
+/**
  * If the given property of the given object is a function,
  * it modifies it to emit events before and after calling it
  * (events "{method}:before" and "{method}")
@@ -23,7 +32,7 @@ function eventizeMethod(object, methodName) {
   var beforeEventName = methodName + ':before';
 
   object[methodName] = function() {
-    var args = arguments;
+    var args = toArray(arguments);
     object.emit(beforeEventName, args, methodName, object);
     var returnValue = originalMethod.apply(this, args);
     object.emit(methodName, args, returnValue, methodName, object);
